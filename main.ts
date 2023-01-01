0e18b3bdcbec5d89545a01bd0a5d025375c3c377
Martin Gardner <magardner@users.noreply.github.com>
2023-01-01 12:08:26 +0000

    Initial commit starting point of communicator.

--- main.ts
+++ main.ts
@@ -1,1 +1,48 @@
-
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . . . .
+        `)
+    radio.sendString("dit")
+    basic.clearScreen()
+})
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "dit") {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else if (receivedString == "dah") {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # # # .
+            . . . . .
+            . . . . .
+            `)
+    } else {
+        basic.clearScreen()
+    }
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # # # .
+        . . . . .
+        . . . . .
+        `)
+    radio.sendString("dah")
+    basic.clearScreen()
+})
+radio.setGroup(1)
+basic.forever(function () {
+	
+})
