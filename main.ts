288887cc28fec2a2c54dc309ceae9533d7013961
Martin Gardner <magardner@users.noreply.github.com>
2023-01-03 17:51:01 +0000

    Long press to logo allows user to change the radio group.

--- main.ts
+++ main.ts
@@ -1,3 +1,24 @@
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    basic.showString("RG=" + convertToText(RadioGroup))
+    while (!(input.pinIsPressed(TouchPin.P2))) {
+        if (RadioGroup >= 1 && RadioGroup <= 255) {
+            if (input.isGesture(Gesture.TiltLeft)) {
+                RadioGroup += -1
+                basic.showNumber(RadioGroup)
+            } else if (input.isGesture(Gesture.TiltRight)) {
+                RadioGroup += 1
+                basic.showNumber(RadioGroup)
+            } else {
+            	
+            }
+        } else {
+            RadioGroup = 1
+            basic.showNumber(RadioGroup)
+        }
+    }
+    radio.setGroup(RadioGroup)
+    basic.showString("RG=" + convertToText(RadioGroup))
+})
 input.onButtonPressed(Button.A, function () {
     basic.showLeds(`
         . . . . .
@@ -42,7 +63,7 @@ input.onButtonPressed(Button.B, function () {
     radio.sendString("dah")
     basic.clearScreen()
 })
-radio.setGroup(1)
-basic.forever(function () {
-	
-})
+let RadioGroup = 0
+RadioGroup = 1
+radio.setGroup(RadioGroup)
+basic.showIcon(IconNames.Happy)
